906fde7034c209e12c2a2a1e5e6fea834ab59dbf
Natthaphong66 <natthaphong.di.66@ubu.ac.th>
2025-12-01 19:18:17 +0700

    Fix security issues

--- ezshop/settings.py
+++ ezshop/settings.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-51g38fv_x8gge461w^mr9x$qgv41d-hw2(adzd=nh_-o(23bh#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-51g38fv_x8gge461w^mr9x$qgv41d-hw2(adzd=nh_-o(23bh#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "1") == "1"
